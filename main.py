36643e0c5991efb3c9317d7ad754045ed20b2ba1
Raphael Taveira <raphaeltav@Raphas-MacBook-Air.local>
2023-03-22 18:54:17 -0400

    added a change

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def decode_password(encoded_password): # decode function made by Raphael
+    """Encodes password by having the digit being shifted down by 3 numbers"""
+
+    password = ""
+    for digit in encoded_password:
+        decoded_digit = str((int(digit) - 3) % 10)
+        password += decoded_digit
+    return password
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
